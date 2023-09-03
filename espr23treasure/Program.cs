9634253fee28fcdc2b2031190563cd1027b11738
KermitThePog <radovan.holcik@gmail.com>
2023-09-03 21:58:49 +0200

    Initial commit

---
+++ espr23treasure/Program.cs
@@ -0,0 +1,7 @@
+﻿using espr23treasure;
+
+var lck = new treasureLock();
+lck.Randomise();
+Console.WriteLine(lck.ToString());
+lck.Press(0b11010000);
+Console.WriteLine(lck.ToString());
---
+++ espr23treasure/treasureLock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace espr23treasure
+{
+    internal class treasureLock
+    {
+        public byte buttons = 0b0000;
+        Random rnd = new Random();
+
+        public void Randomise()
+        {
+            buttons = (byte)rnd.Next(0, 256);
+        }
+
+        public void Spin()
+        {
+            
+        }
+
+        public string ToString()
+        {
+            return Convert.ToString(buttons >> 4, 2).PadLeft(4, '0');
+        }
+
+        public void Press(byte toPress)
+        {
+            buttons = (byte)(buttons ^ toPress);
+        }
+    }
+}
