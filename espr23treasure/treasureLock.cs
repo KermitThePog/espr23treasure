a579d4a77c5d94d71fe8aab45f4339c2b0dc7a14
KermitThePog <radovan.holcik@gmail.com>
2023-09-04 18:42:35 +0200

    Added Spin()

--- espr23treasure/treasureLock.cs
+++ espr23treasure/treasureLock.cs
@@ -1,26 +1,33 @@
-﻿using System.Net.Mime;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.Mime;
+using Convert = System.Convert;
 
 namespace espr23treasure
 {
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
     internal class TreasureLock
     {
-        private byte _buttons;
+        private byte _buttons;  //four most significant bits should remain zero throughout the program
         private readonly Random _rnd = new Random();
 
         public void Randomise()
         {
-            _buttons = (byte)_rnd.Next(0, 16);
+            _buttons = (byte)_rnd.Next(0, 16);  //four least significant bits get randomised
             Console.WriteLine(ToString());
             Check();
         }
 
         public void Spin()
         {
-            
+            var b = (byte)(_buttons + (_buttons << 4));     //1101 -> 1101 1101
+            b = (byte)((b >> _rnd.Next(0, 4)) & 0b1111);    //1101 1101 -> 0011 0111 -> 0000 0111
+            _buttons = b;
+            Console.WriteLine(ToString());
         }
 
         public override string ToString()
         {
+            //Outputs _buttons in base 2 and adds '0' to the beginning to set length to 4 digits
             return Convert.ToString(_buttons, 2).PadLeft(4, '0');
         }
 
@@ -29,10 +36,11 @@ namespace espr23treasure
             _buttons = (byte)(_buttons ^ toPress);
             Check();
             Console.WriteLine(ToString());
-            if (flip)
+            if (flip)   //automatically checks to accomodate the possibility of all buttons being 0
             {
                 Press(0b1111, false);
             }
+            Spin();
         }
 
         public void Check()
